4643b437756419355894fc9c612170dbc2d50474
alinaadburshina <alinaabdurshina@gmail.com>
2019-11-24 16:33:30 +0300

    Add Auth Error Class

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const { JWT_DEV_SECRET } = require('../config');
 
+const Error401 = require('../errors/401-err');
+
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -9,9 +11,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : JWT_DEV_SECRET);
   } catch (err) {
-    err.statusCode = 401;
-    err.message = 'Authorisation required';
-    next(err);
+    throw new Error401('Authorisation required');
   }
   req.user = payload;
 
