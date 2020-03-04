7511c8189cad52a97e50c968ebe1f905468e0e7a
alinaadburshina <alinaabdurshina@gmail.com>
2020-03-04 13:54:44 +0300

    Update CORS

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-const cors = require('cors');
+// const cors = require('cors');
 const { celebrate, Joi, errors } = require('celebrate');
 
 const userRouter = require('./routes/userRouter');
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
 
   next();
 });
-app.use(cors());
+// app.use(cors());
 
 app.set('trust proxy', 1);
 app.use(limiter);
