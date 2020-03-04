567298859ed8f5887137a8f707e475e841a5e7a1
alinaadburshina <alinaabdurshina@gmail.com>
2020-03-04 14:55:29 +0300

    CORS?

--- app.js
+++ app.js
@@ -41,10 +41,26 @@ mongoose
     useFindAndModify: false,
   });
 
+const allowedOrigins = [
+  'http://localhost:8080',
+  'http://newsexplo.gq',
+  'https://newsexplo.gq',
+];
 app.use(cors({
   credentials: true,
-  origin: true,
+  origin: (origin, callback) => {
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not '
+        + 'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
 }));
+
 app.set('trust proxy', 1);
 app.use(limiter);
 app.use(helmet());
