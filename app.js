84b503911cd1b93ce736945f38befa3d375eee69
alinaadburshina <alinaabdurshina@gmail.com>
2020-03-05 22:35:15 +0300

    Keep on CORS'in

--- app.js
+++ app.js
@@ -41,7 +41,25 @@ mongoose
     useFindAndModify: false,
   });
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:8080',
+  'http://newsexplo.gq',
+  'https://newsexplo.gq',
+];
+app.use(cors({
+  credentials: true,
+  origin: (origin, callback) => {
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not '
+        + 'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+}));
 
 app.set('trust proxy', 1);
 app.use(limiter);
