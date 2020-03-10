12472ff4cef857b9545d6646c2686cf1fcfc3855
alinaadburshina <alinaabdurshina@gmail.com>
2020-03-10 17:10:34 +0300

    Add CORS specific origins

--- app.js
+++ app.js
@@ -41,7 +41,21 @@ mongoose
     useFindAndModify: false,
   });
 
-app.use(cors());
+const whitelist = [
+  'https://newsexlpo.gq',
+  'http://newsexplo.gq',
+  'http://localhost:8080',
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+app.use(cors(corsOptions));
 
 app.set('trust proxy', 1);
 app.use(limiter);
